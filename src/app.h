fbc95a6b7cbe17e87bdd7434977ba123b12db53c
Alfred Klomp <git@alfredklomp.com>
2025-08-28 23:21:54 +0200

    main: handle acc/mag data
    Print a single character for each sensor message received.

--- src/app.h
+++ src/app.h
@@ -2,13 +2,30 @@
 
 #include <stdint.h>
 
+#include "acc_mag.h"
 #include "gyro.h"
 
 struct app_sensors {
+
 	struct {
-		uint32_t ndata;
+		uint32_t   ndata;
+		struct acc data[2];
+	} acc;
+
+	struct {
+		uint32_t    ndata;
 		struct gyro data[2];
 	} gyro;
+
+	struct {
+		uint32_t   ndata;
+		struct mag data[2];
+	} mag;
+
+	struct {
+		uint32_t ndata;
+		int16_t  data[2];
+	} temp;
 };
 
 extern void app_step (const struct app_sensors *s);
--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
+#include <string.h>
+
+#include "acc_mag.h"
 #include "app.h"
 #include "clock.h"
 #include "event.h"
 #include "frame.h"
 #include "gyro.h"
+#include "i2c.h"
 #include "led.h"
 #include "usart.h"
 
@@ -11,10 +15,12 @@ init (void)
 {
 	clock_init();
 	frame_init();
+	i2c_init();
 	led_init();
 	usart_init();
 
 	// Reboot the sensors.
+	acc_mag_reboot();
 	gyro_reboot();
 
 	// Give the sensors some time to reboot.
@@ -23,7 +29,21 @@ init (void)
 			__asm volatile("wfe");
 
 	// Configure the sensors.
+	acc_mag_config();
 	gyro_config();
+}
+
+static inline void
+store_data (const void *data, uint32_t *ndata, void *dst, const size_t size)
+{
+	// If two readings are available, discard the oldest one.
+	if (*ndata == 2) {
+		memcpy(dst, dst + size, size);
+		*ndata = 1;
+	}
+
+	// Copy the data to the slot.
+	memcpy(dst + (*ndata)++ * size, data, size);
 }
 
 static void
@@ -35,21 +55,31 @@ loop (void)
 	for (;;) {
 
 		// Gyro data received: copy out the fresh data.
-		if (event_test_and_clear(EVENT_GYRO_RX_FINISHED)) {
-
-			// If two readings are available, move the oldest one
-			// out of the way.
-			if (buf.gyro.ndata == 2) {
-				buf.gyro.data[0] = buf.gyro.data[1];
-				buf.gyro.ndata--;
-			}
-
-			buf.gyro.data[buf.gyro.ndata++ & 1] = *gyro_get_data();
-		}
+		if (event_test_and_clear(EVENT_GYRO_RX_FINISHED))
+			store_data(gyro_get_data(), &buf.gyro.ndata,
+			           buf.gyro.data, sizeof (*buf.gyro.data));
 
 		// Gyro data ready: start a read sequence.
 		if (event_test_and_clear(EVENT_GYRO_DATA_READY))
 			gyro_start_tx();
+
+		// Step the accelerometer/magnetometer state machine.
+		acc_mag_step();
+
+		// Accelerometer data received: copy out the fresh data.
+		if (event_test_and_clear(EVENT_ACC_FINISHED))
+			store_data(acc_mag_get_acc(), &buf.acc.ndata,
+			           buf.acc.data, sizeof (*buf.acc.data));
+
+		// Temperature data received: copy out the fresh data.
+		if (event_test_and_clear(EVENT_ACC_TEMP_FINISHED))
+			store_data(acc_mag_get_temp(), &buf.temp.ndata,
+			           buf.temp.data, sizeof (*buf.temp.data));
+
+		// Magnetometer data received: copy out the fresh data.
+		if (event_test_and_clear(EVENT_MAG_FINISHED))
+			store_data(acc_mag_get_mag(), &buf.mag.ndata,
+			           buf.mag.data, sizeof (*buf.mag.data));
 
 		// Step the application on every frame with sensor data.
 		if (event_test_and_clear(EVENT_FRAME)) {
@@ -58,7 +88,10 @@ loop (void)
 			app_step(&buf);
 
 			// Reset the counters.
+			buf.acc.ndata  = 0;
 			buf.gyro.ndata = 0;
+			buf.mag.ndata  = 0;
+			buf.temp.ndata = 0;
 		}
 
 		// Sleep until the next event occurs.
